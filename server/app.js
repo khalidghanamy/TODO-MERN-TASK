3a44af99cefb8ab03f3ae35cd13db8ec2bbb868a
khalidghanamy <khalid.gamal.hamed@gmail.com>
2022-07-03 15:22:54 +0200

    update deploy

--- server/app.js
+++ server/app.js
@@ -50,9 +50,9 @@ console.log(dirname.pop());
 console.log('directory-name 👉️', __dirname);
 
 if(process.env.NODE_ENV==="production"){
-    app.use(express.static(path.join(__dirname,"client/build")));
+    app.use(express.static(path.join(__dirname,"build")));
     app.get("*",(req,res)=>{
-       return path.resolve(__dirname,"client","build","index.html");
+       return path.resolve(__dirname,"build","index.html");
     }
     )
 }
