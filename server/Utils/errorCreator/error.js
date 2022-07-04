864cdede5bc4bd9e521c1e25ffc018e42e925e82
khalidghanamy <khalid.gamal.hamed@gmail.com>
2022-07-05 00:17:08 +0200

    fix auth With Jwt and fix front Many changes

--- server/Utils/errorCreator/error.js
+++
@@ -1,8 +0,0 @@
-
-
-export const CreateError = (status, message) => {
-        const error = new Error();
-        error.statusCode = status;
-        error.message = message;
-        return error;
-    }
--- server/app.js
+++ server/app.js
@@ -53,7 +53,7 @@ if(process.env.NODE_ENV==="production"){
     app.get("*",(req,res)=>{
         res.sendFile(path.join(__dirname,"/build/index.html"));
         // console.log(path.resolve(__dirname,"client","build","index.html"));
-    }
+    } 
     )
 }
 //====> Middleware <========================================
